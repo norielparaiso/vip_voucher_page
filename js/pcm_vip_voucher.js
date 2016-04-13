0e15b891fc3f6166878be491ff4c3f209ba85cea
Noriel <noriel.paraiso@pcm.com>
2016-04-13 08:48:30 +0800

    tooltwist integ

--- js/pcm_vip_voucher.js
+++ js/pcm_vip_voucher.js
@@ -74,7 +74,10 @@ PCM.vipVoucherScript = function() {
 
 				case "13810041":
 					val_prodName = 'HP Inc. Smart Buy ProDesk 400 G3 Intel Core i7-6700 Quad-Core 3.40GHz Microtower PC - 8GB RAM, 1TB HDD, Slim SuperMulti DVD, Gigabit Ethernet';
-					val_thumbnails = '';
+					val_thumbnails = '<img src="http://i2.cc-inc.com/prod/13238000/13810041_ti_0.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13810041_xxlg_0.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13810041_ti_1.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13810041_xxlg_1.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13810041_ti_2.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13810041_xxlg_2.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13810041_ti_3.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13810041_xxlg_3.jpg" class="thumb">';
 					val_price = '$699<sup>99</sup>';
 					val_listPrice = '$899<sup>99</sup>';
 					val_discount = '$200';
@@ -86,119 +89,152 @@ PCM.vipVoucherScript = function() {
 					break;
 
 				case "13382163":
-					val_prodName = '';
-					val_thumbnails = '';
-					val_price = '';
-					val_listPrice = '';
-					val_discount = '';
+					val_prodName = 'HP Inc. V241P - LED monitor - 23.6" - 1920 x 1080 - 250 cd/m2 - 3000:1 - 8 ms - DVI-D, VGA - black - Smart Buy';
+					val_thumbnails = '<img src="http://i2.cc-inc.com/prod/13238000/13382163_ti_0.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13382163_xxlg_0.jpg" class="thumb">';
+					val_price = '$129<sup>99</sup>';
+					val_listPrice = '$149<sup>99</sup>';
+					val_discount = '$20';
 					isValid = true;
 					break;
 
 				case "13816882":
-					val_prodName = '';
-					val_thumbnails = '';
-					val_price = '';
-					val_listPrice = '';
-					val_discount = '';
+					val_prodName = 'HP Inc. V193b - LED monitor - 18.5" - 1366 x 768 - 200 cd/m2 - 600:1 - 5 ms - VGA - black - VGA (HD-15)';
+					val_thumbnails = '<img src="http://i2.cc-inc.com/prod/13238000/13816882_ti_0.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13816882_xxlg_0.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13816882_ti_1.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13816882_xxlg_1.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13816882_ti_2.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13816882_xxlg_2.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13816882_ti_3.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13816882_xxlg_3.jpg" class="thumb">';
+					val_price = '$76<sup>99</sup>';
+					val_listPrice = '$109<sup>99</sup>';
+					val_discount = '$33';
 					isValid = true;
 					break;
 
 				case "13756489":
-					val_prodName = '';
-					val_thumbnails = '';
-					val_price = '';
-					val_listPrice = '';
-					val_discount = '';
+					val_prodName = 'HP Inc. V222 - LED monitor - 21.5" - 1920 x 1080 - TN - 200 cd/m2 - 600:1 - 5 ms - DVI-D, VGA - black - promo';
+					val_thumbnails = '<img src="http://i2.cc-inc.com/prod/13238000/13756489_ti_0.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13756489_xxlg_0.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13756489_ti_1.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13756489_xxlg_1.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13756489_ti_2.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13756489_xxlg_2.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13756489_ti_3.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13756489_xxlg_3.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13756489_ti_4.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13756489_xxlg_4.jpg" class="thumb">';
+					val_price = '$99<sup>99</sup>';
+					val_listPrice = '$149<sup>99</sup>';
+					val_discount = '$50';
 					isValid = true;
 					break;
 
 				case "13787937":
-					val_prodName = '';
-					val_thumbnails = '';
-					val_price = '';
-					val_listPrice = '';
-					val_discount = '';
+					val_prodName = 'HP Inc. 280 G1 - Slim tower - 1 x Core i3 4170 / 3.7 GHz - RAM 4 GB - HDD 500 GB - DVD SuperMulti - HD Graphics 4400 - GigE - Microsoft Windows 7 Professional 64-bit Edition';
+					val_thumbnails = '<img src="http://i2.cc-inc.com/prod/13238000/13787937_ti_0.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13787937_xxlg_0.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13787937_ti_1.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13787937_xxlg_1.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13787937_ti_2.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13787937_xxlg_2.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13787937_ti_3.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13787937_xxlg_3.jpg" class="thumb">';
+					val_price = '$371<sup>99</sup>';
+					val_listPrice = '$429<sup>99</sup>';
+					val_discount = '$58';
 					isValid = true;
 					break;
 
 				case "13685097":
-					val_prodName = '';
-					val_thumbnails = '';
-					val_price = '';
-					val_listPrice = '';
-					val_discount = '';
+					val_prodName = 'HP Inc. 250 G4 - Core i3 5005U / 2 GHz - Win 7 Pro 64-bit - 4 GB RAM - 500 GB HDD - DVD SuperMulti - 15.6" 1366 x 768 ( HD ) - HD Graphics 5500 - black ';
+					val_thumbnails = '<img src="http://i2.cc-inc.com/prod/13238000/13685097_ti_0.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13685097_xxlg_0.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13685097_ti_1.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13685097_xxlg_1.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13685097_ti_2.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13685097_xxlg_2.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13685097_ti_4.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13685097_xxlg_4.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13685097_ti_3.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13685097_xxlg_3.jpg" class="thumb">';
+					val_price = '$329<sup>99</sup>';
+					val_listPrice = '$429<sup>99</sup>';
+					val_discount = '$100';
 					isValid = true;
 					break;
 
-				case "13238735":
-					val_prodName = '';
-					val_thumbnails = '';
-					val_price = '';
-					val_listPrice = '';
-					val_discount = '';
+				case "13238735": // DUPLICATE SKU, PLEASE RECHECK WITH THE DESIGNER
 					isValid = true;
 					break;
 
 				case "13184749":
-					val_prodName = '';
-					val_thumbnails = '';
-					val_price = '';
-					val_listPrice = '';
-					val_discount = '';
+					val_prodName = 'HP Inc. Workstation Z230 - MT - 1 x Core i7 4790 / 3.6 GHz - RAM 8 GB - HDD 1 TB - DVD SuperMulti - HD Graphics 4600 - GigE - Win 8.1 Pro 64-bit / Win 7 Pro 64-bit downgrade';
+					val_thumbnails = '<img src="http://i2.cc-inc.com/prod/13238000/13184749_ti_0.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13184749_xxlg_0.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13184749_ti_1.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13184749_xxlg_1.jpg" class="thumb">';
+					val_price = '$799<sup>99</sup>';
+					val_listPrice = '$1,199<sup>99</sup>';
+					val_discount = '$400';
 					isValid = true;
 					break;
 
 				case "13848731":
-					val_prodName = '';
-					val_thumbnails = '';
-					val_price = '';
-					val_listPrice = '';
-					val_discount = '';
+					val_prodName = 'HP Inc. Smart Buy ProBook 450 G3 Intel Core i7-6500U Dual-Core 2.50GHz Notebook PC - 8GB RAM, 1TB HDD, 15.6" FHD LED, DVD+/-RW SuperMulti';
+					val_thumbnails = '<img src="http://i2.cc-inc.com/prod/13238000/13848731_ti_0.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13848731_xxlg_0.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13848731_ti_1.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13848731_xxlg_1.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13848731_ti_2.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13848731_xxlg_2.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13848731_ti_3.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13848731_xxlg_3.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13848731_ti_4.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13848731_xxlg_4.jpg" class="thumb">';
+					val_price = '$699<sup>99</supp>';
+					val_listPrice = '$899<sup>99</supp>';
+					val_discount = '$200';
 					isValid = true;
 					break;
 
 				case "13848732":
-					val_prodName = '';
-					val_thumbnails = '';
-					val_price = '';
-					val_listPrice = '';
-					val_discount = '';
+					val_prodName = 'HP Inc. Smart Buy ProBook 450 G3 Intel Core i3-6100U Dual-Core 2.30GHz Notebook PC - 8GB RAM, 500GB HDD, 15.6" HD LED, DVD+/-RW SuperMulti, Gigabit Ethernet';
+					val_thumbnails = '<img src="http://i2.cc-inc.com/prod/13238000/13848732_ti_0.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13848732_xxlg_0.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13848732_ti_1.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13848732_xxlg_1.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13848732_ti_2.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13848732_xxlg_2.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13848732_ti_3.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13848732_xxlg_3.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13848732_ti_4.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13848732_xxlg_4.jpg" class="thumb">';
+					val_price = '$529<sup>99</supp>';
+					val_listPrice = '$649<sup>99</supp>';
+					val_discount = '$121';
 					isValid = true;
 					break;
 
 				case "13575801":
-					val_prodName = '';
-					val_thumbnails = '';
-					val_price = '';
-					val_listPrice = '';
-					val_discount = '';
+					val_prodName = 'HP Inc. Chromebook 11 G4 - Celeron N2840 / 2.16 GHz - Chrome OS - 4 GB RAM - 16 GB eMMC - 11.6" TN 1366 x 768 ( HD ) - HD Graphics';
+					val_thumbnails = '<img src="http://i2.cc-inc.com/prod/13238000/13575801_ti_0.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13575801_xxlg_0.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13575801_ti_1.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13575801_xxlg_1.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13575801_ti_2.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13575801_xxlg_2.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13575801_ti_3.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13575801_xxlg_3.jpg" class="thumb">';
+					val_price = '$120';
+					val_listPrice = '$1,499<sup>99</supp>';
+					val_discount = '$159';
 					isValid = true;
 					break;
 
-				case "13405392":
-					val_prodName = '';
-					val_thumbnails = '';
-					val_price = '';
-					val_listPrice = '';
-					val_discount = '';
+				case "13405392": // CAN'T FIND ANY IMAGES OF THIS PRODUCT IN THE WEBSITE
+					val_prodName = 'HP Inc. Elite x2 1011 G1 - Ultrabook - Core M 5Y51 / 1.1 GHz - Win 10 Pro 64-bit - 8 GB RAM - 256 GB SSD - no ODD - 11.6" IPS touchscreen 1920 x 1080 ( Full HD ) - HD Graphics 5300 - 802.11ac, 802.11ad (WiGig) - with Elite x2 1011 G1 Power Keyboard';
+					val_thumbnails = '<img src="http://i2.cc-inc.com/prod/13238000/13405392_ti_0.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13405392_xxlg_0.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13405392_ti_1.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13405392_xxlg_1.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13405392_ti_2.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13405392_xxlg_2.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13405392_ti_3.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13405392_xxlg_3.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13405392_ti_4.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13405392_xxlg_4.jpg" class="thumb">';
+					val_price = '$1,459<sup>99</supp>';
+					val_listPrice = '$1,499<sup>99</supp>';
+					val_discount = '$159';
 					isValid = true;
 					break;
 
 				case "13405359":
-					val_prodName = '';
-					val_thumbnails = '';
-					val_price = '';
-					val_listPrice = '';
-					val_discount = '';
+					val_prodName = 'HP Inc. Elite x2 1011 G1 - Tablet - no keyboard - Core M 5Y51 / 1.1 GHz - Win 8.1 64-bit - 4 GB RAM - 128 GB SSD - 11.6" IPS touchscreen 1920 x 1080';
+					val_thumbnails = '<img src="http://i2.cc-inc.com/prod/13238000/13405359_ti_0.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13405359_xxlg_0.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13405359_ti_1.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13405359_xxlg_1.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13405359_ti_2.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13405359_xxlg_2.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13405359_ti_3.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13405359_xxlg_3.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13405359_ti_4.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13405359_xxlg_4.jpg" class="thumb">';
+					val_price = '$1,014<sup>99</supp>';
+					val_listPrice = '$1,499<sup>99</supp>';
+					val_discount = '$159';
 					isValid = true;
 					break;
 
 				case "13405387":
-					val_prodName = '';
-					val_thumbnails = '';
-					val_price = '';
-					val_listPrice = '';
-					val_discount = '';
+					val_prodName = 'HP Inc. Elite x2 1011 G1 - Tablet - with keyboard dock - Core M 5Y10c / 800 MHz - Win 8.1 Pro 64-bit - 4 GB RAM - 128 GB SSD - 11.6" IPS touchscreen 1920 x 1080 ( Full HD )';
+					val_thumbnails = '<img src="http://i2.cc-inc.com/prod/13238000/13405387_ti_0.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13405387_xxlg_0.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13405387_ti_1.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13405387_xxlg_1.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13405387_ti_2.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13405387_xxlg_2.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13405387_ti_3.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13405387_xxlg_3.jpg" class="thumb">'
+									+'<img src="http://i2.cc-inc.com/prod/13238000/13405387_ti_4.jpg" data-large="http://i2.cc-inc.com/prod/13238000/13405387_xxlg_4.jpg" class="thumb">';
+					val_price = '$949<sup>99</supp>';
+					val_listPrice = '$1,499<sup>99</supp>';
+					val_discount = '$159';
 					isValid = true;
 					break;
 
--- landing.php
+++ landing.php
@@ -2,45 +2,91 @@
 <div class="prod-listing">
 	<div class="each-row">
 		<div class="each-col">
-			<h3 class="prodName">HP Inc. EliteBook Folio 9480m Core i5 4310U / 2 GHz </h3>
+			<h3 class="prodName">HP Inc. EliteBook Folio 9480m - Core i5 4310U / 2 GHz - Win 7 Pro 64-bit - 4 GB RAM - 256 GB SSD Self Encrypting Drive - no ODD - 14" 1600 x 900 ( HD+ )</h3>
 			<div class="prodImg">
-				<img src="http://i2.cc-inc.com/prod/13238000/13238735_lg_0.jpg" alt="">
+				<img src="http://i2.cc-inc.com/prod/13238000/13238735_xxlg_0.jpg" alt="">
 			</div>
 			<div class="pricing">
-				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $700</span></p>
+				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $159</span></p>
 				<p class="price">$799<sup>99</sup></p>
 				<p class="partNo">Part # 13238735</p>
 			</div>
 		</div>
 		<div class="each-col">
-			<h3 class="prodName">HP Inc. EliteBook Folio 9480m Core i5 4310U / 2 GHz </h3>
+			<h3 class="prodName">HP Inc. Smart Buy ProDesk 400 G3 Intel Core i7-6700 Quad-Core 3.40GHz Microtower PC - 8GB RAM, 1TB HDD, Slim SuperMulti DVD, Gigabit Ethernet</h3>
 			<div class="prodImg">
-				<img src="http://i2.cc-inc.com/prod/13238000/13238735_lg_0.jpg" alt="">
+				<img src="http://i2.cc-inc.com/prod/13238000/13810041_xxlg_0.jpg" alt="">
 			</div>
 			<div class="pricing">
-				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $700</span></p>
-				<p class="price">$799<sup>99</sup></p>
+				<p class="save">$899<sup>99</sup> &rsaquo; <span>Save $200</span></p>
+				<p class="price">$699<sup>99</sup></p>
 				<p class="partNo">Part # 13238735</p>
 			</div>
 		</div>
 		<div class="each-col">
-			<h3 class="prodName">HP Inc. EliteBook Folio 9480m Core i5 4310U / 2 GHz </h3>
+			<h3 class="prodName">HP Inc. V241P - LED monitor - 23.6" - 1920 x 1080 - 250 cd/m2 - 3000:1 - 8 ms - DVI-D, VGA - black - Smart Buy</h3>
 			<div class="prodImg">
-				<img src="http://i2.cc-inc.com/prod/13238000/13238735_lg_0.jpg" alt="">
+				<img src="http://i2.cc-inc.com/prod/13238000/13382163_xxlg_0.jpg" alt="">
 			</div>
 			<div class="pricing">
-				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $700</span></p>
-				<p class="price">$799<sup>99</sup></p>
+				<p class="save">$149<sup>99</sup> &rsaquo; <span>Save $20</span></p>
+				<p class="price">$129<sup>99</sup></p>
 				<p class="partNo">Part # 13238735</p>
 			</div>
 		</div>
 		<div class="each-col">
-			<h3 class="prodName">HP Inc. EliteBook Folio 9480m Core i5 4310U / 2 GHz </h3>
+			<h3 class="prodName">HP Inc. V193b - LED monitor - 18.5" - 1366 x 768 - 200 cd/m2 - 600:1 - 5 ms - VGA - black - VGA (HD-15)</h3>
 			<div class="prodImg">
-				<img src="http://i2.cc-inc.com/prod/13238000/13238735_lg_0.jpg" alt="">
+				<img src="http://i2.cc-inc.com/prod/13238000/13816882_xxlg_0.jpg" alt="">
 			</div>
 			<div class="pricing">
-				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $700</span></p>
+				<p class="save">$109<sup>99</sup> &rsaquo; <span>Save $33</span></p>
+				<p class="price">$76<sup>99</sup></p>
+				<p class="partNo">Part # 13238735</p>
+			</div>
+		</div>
+	</div>
+	<div class="each-row">
+		<div class="each-col">
+			<h3 class="prodName">HP Inc. V222 - LED monitor - 21.5" - 1920 x 1080 - TN - 200 cd/m2 - 600:1 - 5 ms - DVI-D, VGA - black - promo</h3>
+			<div class="prodImg">
+				<img src="http://i2.cc-inc.com/prod/13238000/13756489_xxlg_0.jpg" alt="">
+			</div>
+			<div class="pricing">
+				<p class="save">$149<sup>99</sup> &rsaquo; <span>Save $50</span></p>
+				<p class="price">$99<sup>99</sup></p>
+				<p class="partNo">Part # 13238735</p>
+			</div>
+		</div>
+		<div class="each-col">
+			<h3 class="prodName">HP Inc. 280 G1 - Slim tower - 1 x Core i3 4170 / 3.7 GHz - RAM 4 GB - HDD 500 GB - DVD SuperMulti - HD Graphics 4400 - GigE - Microsoft Windows 7 Professional 64-bit Edition</h3>
+			<div class="prodImg">
+				<img src="http://i2.cc-inc.com/prod/13238000/13787937_xxlg_0.jpg" alt="">
+			</div>
+			<div class="pricing">
+				<p class="save">$429<sup>99</sup> &rsaquo; <span>Save $58</span></p>
+				<p class="price">$371<sup>99</sup></p>
+				<p class="partNo">Part # 13238735</p>
+			</div>
+		</div>
+		<div class="each-col">
+			<h3 class="prodName">HP Inc. 250 G4 - Core i3 5005U / 2 GHz - Win 7 Pro 64-bit - 4 GB RAM - 500 GB HDD - DVD SuperMulti - 15.6" 1366 x 768 ( HD ) - HD Graphics 5500 - black</h3>
+			<div class="prodImg">
+				<img src="http://i2.cc-inc.com/prod/13238000/13685097_xxlg_0.jpg" alt="">
+			</div>
+			<div class="pricing">
+				<p class="save">$429<sup>99</sup> &rsaquo; <span>Save $100</span></p>
+				<p class="price">$329<sup>99</sup></p>
+				<p class="partNo">Part # 13238735</p>
+			</div>
+		</div>
+		<div class="each-col">
+			<h3 class="prodName">HP Inc. Workstation Z230 - MT - 1 x Core i7 4790 / 3.6 GHz - RAM 8 GB - HDD 1 TB - DVD SuperMulti - HD Graphics 4600 - GigE - Win 8.1 Pro 64-bit / Win 7 Pro 64-bit downgrade</h3>
+			<div class="prodImg">
+				<img src="http://i2.cc-inc.com/prod/13238000/13184749_xxlg_0.jpg" alt="">
+			</div>
+			<div class="pricing">
+				<p class="save">$1,199<sup>99</sup> &rsaquo; <span>Save $400</span></p>
 				<p class="price">$799<sup>99</sup></p>
 				<p class="partNo">Part # 13238735</p>
 			</div>
@@ -48,92 +94,70 @@
 	</div>
 	<div class="each-row">
 		<div class="each-col">
-			<h3 class="prodName">HP Inc. EliteBook Folio 9480m Core i5 4310U / 2 GHz </h3>
+			<h3 class="prodName">HP Inc. Smart Buy ProBook 450 G3 Intel Core i7-6500U Dual-Core 2.50GHz Notebook PC - 8GB RAM, 1TB HDD, 15.6" FHD LED, DVD+/-RW SuperMulti</h3>
 			<div class="prodImg">
-				<img src="http://i2.cc-inc.com/prod/13238000/13238735_lg_0.jpg" alt="">
+				<img src="http://i2.cc-inc.com/prod/13238000/13848731_xxlg_0.jpg" alt="">
 			</div>
 			<div class="pricing">
-				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $700</span></p>
-				<p class="price">$799<sup>99</sup></p>
+				<p class="save">$899<sup>99</sup> &rsaquo; <span>Save $200</span></p>
+				<p class="price">$699<sup>99</sup></p>
 				<p class="partNo">Part # 13238735</p>
 			</div>
 		</div>
 		<div class="each-col">
-			<h3 class="prodName">HP Inc. EliteBook Folio 9480m Core i5 4310U / 2 GHz </h3>
+			<h3 class="prodName">HP Inc. Smart Buy ProBook 450 G3 Intel Core i3-6100U Dual-Core 2.30GHz Notebook PC - 8GB RAM, 500GB HDD, 15.6" HD LED, DVD+/-RW SuperMulti, Gigabit Ethernet</h3>
 			<div class="prodImg">
-				<img src="http://i2.cc-inc.com/prod/13238000/13238735_lg_0.jpg" alt="">
+				<img src="http://i2.cc-inc.com/prod/13238000/13848732_xxlg_0.jpg" alt="">
 			</div>
 			<div class="pricing">
-				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $700</span></p>
-				<p class="price">$799<sup>99</sup></p>
+				<p class="save">$649<sup>99</sup> &rsaquo; <span>Save $121</span></p>
+				<p class="price">$529<sup>99</sup></p>
 				<p class="partNo">Part # 13238735</p>
 			</div>
 		</div>
 		<div class="each-col">
-			<h3 class="prodName">HP Inc. EliteBook Folio 9480m Core i5 4310U / 2 GHz </h3>
+			<h3 class="prodName">HP Inc. Chromebook 11 G4 - Celeron N2840 / 2.16 GHz - Chrome OS - 4 GB RAM - 16 GB eMMC - 11.6" TN 1366 x 768 ( HD ) - HD Graphics</h3>
 			<div class="prodImg">
-				<img src="http://i2.cc-inc.com/prod/13238000/13238735_lg_0.jpg" alt="">
+				<img src="http://i2.cc-inc.com/prod/13238000/13575801_xxlg_0.jpg" alt="">
 			</div>
 			<div class="pricing">
-				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $700</span></p>
-				<p class="price">$799<sup>99</sup></p>
+				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $159</span></p>
+				<p class="price">$120</p>
 				<p class="partNo">Part # 13238735</p>
 			</div>
 		</div>
 		<div class="each-col">
-			<h3 class="prodName">HP Inc. EliteBook Folio 9480m Core i5 4310U / 2 GHz </h3>
+			<h3 class="prodName">HP Inc. Elite x2 1011 G1 - Ultrabook - Core M 5Y51 / 1.1 GHz - Win 10 Pro 64-bit - 8 GB RAM - 256 GB SSD - no ODD - 11.6" IPS touchscreen 1920 x 1080 ( Full HD ) - HD Graphics 5300 - 802.11ac, 802.11ad (WiGig) - with Elite x2 1011 G1 Power Keyboard</h3>
 			<div class="prodImg">
-				<img src="http://i2.cc-inc.com/prod/13238000/13238735_lg_0.jpg" alt="">
+				<img src="http://i2.cc-inc.com/prod/13238000/13405392_xxlg_0.jpg" alt="">
 			</div>
 			<div class="pricing">
-				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $700</span></p>
-				<p class="price">$799<sup>99</sup></p>
+				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $159</span></p>
+				<p class="price">$1,459<sup>99</sup></p>
 				<p class="partNo">Part # 13238735</p>
 			</div>
 		</div>
 	</div>
 	<div class="each-row">
 		<div class="each-col">
-			<h3 class="prodName">HP Inc. EliteBook Folio 9480m Core i5 4310U / 2 GHz </h3>
+			<h3 class="prodName">HP Inc. Elite x2 1011 G1 - Tablet - no keyboard - Core M 5Y51 / 1.1 GHz - Win 8.1 64-bit - 4 GB RAM - 128 GB SSD - 11.6" IPS touchscreen 1920 x 1080</h3>
 			<div class="prodImg">
-				<img src="http://i2.cc-inc.com/prod/13238000/13238735_lg_0.jpg" alt="">
+				<img src="http://i2.cc-inc.com/prod/13238000/13405359_xxlg_0.jpg" alt="">
 			</div>
 			<div class="pricing">
-				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $700</span></p>
-				<p class="price">$799<sup>99</sup></p>
+				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $159</span></p>
+				<p class="price">$1,014<sup>99</sup></p>
 				<p class="partNo">Part # 13238735</p>
 			</div>
 		</div>
 		<div class="each-col">
-			<h3 class="prodName">HP Inc. EliteBook Folio 9480m Core i5 4310U / 2 GHz </h3>
+			<h3 class="prodName">HP Inc. Elite x2 1011 G1 - Tablet - with keyboard dock - Core M 5Y10c / 800 MHz - Win 8.1 Pro 64-bit - 4 GB RAM - 128 GB SSD - 11.6" IPS touchscreen 1920 x 1080 ( Full HD )</h3>
 			<div class="prodImg">
-				<img src="http://i2.cc-inc.com/prod/13238000/13238735_lg_0.jpg" alt="">
+				<img src="http://i2.cc-inc.com/prod/13238000/13405387_xxlg_0.jpg" alt="">
 			</div>
 			<div class="pricing">
-				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $700</span></p>
-				<p class="price">$799<sup>99</sup></p>
-				<p class="partNo">Part # 13238735</p>
-			</div>
-		</div>
-		<div class="each-col">
-			<h3 class="prodName">HP Inc. EliteBook Folio 9480m Core i5 4310U / 2 GHz </h3>
-			<div class="prodImg">
-				<img src="http://i2.cc-inc.com/prod/13238000/13238735_lg_0.jpg" alt="">
-			</div>
-			<div class="pricing">
-				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $700</span></p>
-				<p class="price">$799<sup>99</sup></p>
-				<p class="partNo">Part # 13238735</p>
-			</div>
-		</div>
-		<div class="each-col">
-			<h3 class="prodName">HP Inc. EliteBook Folio 9480m Core i5 4310U / 2 GHz </h3>
-			<div class="prodImg">
-				<img src="http://i2.cc-inc.com/prod/13238000/13238735_lg_0.jpg" alt="">
-			</div>
-			<div class="pricing">
-				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $700</span></p>
-				<p class="price">$799<sup>99</sup></p>
+				<p class="save">$1,499<sup>99</sup> &rsaquo; <span>Save $159</span></p>
+				<p class="price">$949<sup>99</sup></p>
 				<p class="partNo">Part # 13238735</p>
 			</div>
 		</div>
